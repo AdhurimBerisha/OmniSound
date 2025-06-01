b68ce7a30d3325cf17bba42a4e002bb1e009ee45
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-06-02 00:08:15 +0200

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Hello World</h1>
+      <Button variant={"outline"}>This is a button</Button>
+    </>
+  );
+}
+
+export default App;
