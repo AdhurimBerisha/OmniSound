709bf76cd1fcee8060fea5d48b18f2faa434a15e
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-06-02 18:39:43 +0200

    createAlbum function

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -73,3 +73,28 @@ export const deleteSong = async (req, res, next) => {
     next(error);
   }
 };
+
+export const createAlbum = async (req, res, next) => {
+  try {
+    const { title, artist, releaseYear } = req.body;
+    const { imageFile } = req.files;
+
+    const imageUrl = await uploadToCloudinary(imageFile);
+
+    const album = new Album({
+      title,
+      artist,
+      imageUrl,
+      releaseYear,
+    });
+
+    await album.save();
+
+    res.status(201).json(album);
+  } catch (error) {
+    console.log("Error in createAlbum", error);
+    next(error);
+  }
+};
+
+export const deleteAlbum = async (req, res, next) => {};
