6667d16d6318197955530c9e6beff877c35aca13
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-06-05 18:57:57 +0200

    Typo

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -19,6 +19,6 @@ router.post("/songs", createSong);
 router.delete("/songs/:id", deleteSong);
 
 router.post("/albums", createAlbum);
-router.delete("albums/:id", deleteAlbum);
+router.delete("/albums/:id", deleteAlbum);
 
 export default router;
